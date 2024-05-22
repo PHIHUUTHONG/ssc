5a8dc5ea766faf4622c513142cd59d9825f62839
Kẹo Kéo <kakathic@gmail.com>
2024-05-23 00:35:25 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 sudo apt install g++-aarch64-linux-gnu binutils-aarch64-linux-gnu
 export CROSS_COMPILE=aarch64-linux-gnu-
-./ssc -i /system/bin/sh echo.sh kaka.sh
+./ssc -e libandroid-wordexp.tar.gz echo.sh kaka.sh
 #./ssc -s echo.sh kaka.sh
