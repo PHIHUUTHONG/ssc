93d77bb033890434f267c2aa14853de29e1b2541
Kẹo Kéo <kakathic@gmail.com>
2024-05-23 22:43:14 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 
 sudo apt install g++-aarch64-linux-gnu binutils-aarch64-linux-gnu
 
-CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-gnu-
 
-. ssc -s echo.sh kaka.sh
+./ssc -s echo.sh kaka.sh
